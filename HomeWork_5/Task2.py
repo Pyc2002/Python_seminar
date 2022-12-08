12c0c913a98e444daf236be4de91be8134283b86
Chirak <Pyc2002@yandex.ru>
2022-12-08 10:49:36 +0300

    Task 2 - Game with sweets

---
+++ HomeWork_5/Task2.py
@@ -0,0 +1,86 @@
+# 2- Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+import random
+
+def The_game(max_sweet, num):
+    gamer_1 = 0
+    gamer_2 = 0
+    first = random.randint(0,2)
+    if first: print('Первым ходит игрок 1')
+    else: print('Первым ходит игрок 2')
+    while max_sweet > 0:
+        sweets = int(input(f'Введите количество конфет, не более {num} шт.:\n'))
+        while sweets < 1 or sweets > num:
+            sweets = int(input(f'ОШИБКА! Количество конфет должно быть от 1 до {num} шт.:\n'))
+        if first:
+            gamer_1 += sweets
+            max_sweet -= sweets
+            first = 0
+            if max_sweet <= 0: 
+                print('Победил игрок 1, все конфеты достаются ему!')
+                break
+            else:
+                print(f'У игрока 1 всего: {gamer_1} конфет')
+                print(f'На столе осталось {max_sweet} конфет')
+                print('        ')
+                print('Игрок 2, Ваша очередь!')
+        else:
+            gamer_2 += sweets
+            max_sweet -= sweets
+            first = 1
+            if max_sweet <= 0: 
+                print('Победил игрок 2, все конфеты достаются ему!')
+                break
+            else:
+                print(f'У игрока 2 всего: {gamer_2} конфет')
+                print(f'На столе осталось {max_sweet} конфет')
+                print('        ')
+                print('Игрок 1, Ваша очередь!')
+
+
+def The_game_bot(max_sweet, num):
+    gamer_1 = 0
+    bot = 0
+    first = random.randint(0,2)
+    if first: print('Первым ходит игрок')
+    else: print('Первым ходит компьютер')
+    while max_sweet > 0:
+        if first:
+            sweets = int(input(f'Введите количество конфет, не более {num} шт.:\n'))
+            while sweets < 1 or sweets > num:
+                sweets = int(input(f'ОШИБКА! Количество конфет должно быть от 1 до {num} шт.:\n'))
+            gamer_1 += sweets
+            max_sweet -= sweets
+            first = 0
+            if max_sweet <= 0: 
+                print('Победил игрок, все конфеты достаются ему!')
+                break
+            else:
+                print(f'У игрока всего: {gamer_1} конфет')
+                print(f'На столе осталось {max_sweet} конфет')
+                print('        ')
+        else:
+            sweets = max_sweet % (num + 1)
+            bot += sweets
+            max_sweet -= sweets
+            first = 1
+            if max_sweet <= 0: 
+                print('Победил компьютер, все конфеты достаются ему!')
+                break
+            else:
+                print (f'Компьютер взял {sweets} конфет')
+                print(f'У компьютера всего: {bot} конфет')
+                print(f'На столе осталось {max_sweet} конфет')
+                print('        ')
+                print('Игрок, Ваша очередь!')
+
+number_of_sweets = abs(int(input('Введите общее количество конфет:\n')))
+sweets_per_round = abs(int(input('Введите максимальное количество конфет, которое можно забрать за 1 ход:\n')))
+who_plays = input('Выберете с кем будете играть:\n С человеком - нажмите 1\n С компьютером - нажмите 2\n')
+if who_plays == '1': The_game(number_of_sweets, sweets_per_round)
+else: The_game_bot(number_of_sweets, sweets_per_round)
